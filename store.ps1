d15a0b7bc62686ddaa530f97f2346395d0e5b47b
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-01-29 00:34:18 +0330

    Update store.ps1

--- store.ps1
+++ store.ps1
@@ -1,1 +1,4 @@
+Get-AppxPackage
+Get-AppxPackage | Select Name , PackageFullName
 Get-AppxPackage -AllUsers | Remove-AppxPackage
+Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online
