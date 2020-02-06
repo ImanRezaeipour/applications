4a8f3acbcf9d967de0e8a5610beb490eaeb1a6ae
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-06 15:00:52 +0330

    Update choco.ps1

--- choco.ps1
+++ choco.ps1
@@ -2,7 +2,14 @@
 choco install -y foxitreader
 choco install -y kis
 
-##### DEVELOP #####
+##### DEVELOP (CLI + Cross Platform + Opern Source)
 choco install -y powershell-core
 choco install -y openssh
 choco install -y vscode
+choco install -y docker-desktop
+choco install -y typescript
+choco install -y dotnetcore-sdk
+choco install -y mongodb
+choco install -y nodejs
+choco install -y nginx
+choco install -y git
