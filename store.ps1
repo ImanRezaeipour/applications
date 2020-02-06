9c9316f86d387137280ba7c156f1327889ba434c
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-06 13:50:57 +0330

    Update store.ps1

--- store.ps1
+++ store.ps1
@@ -2,3 +2,4 @@ Get-AppxPackage
 Get-AppxPackage | Select Name , PackageFullName
 Get-AppxPackage -AllUsers | Remove-AppxPackage
 Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online
+Get-AppXPackage *WindowsStore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
