a3db61612878e018a91df974f152b4e2eae32b26
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-06 19:43:23 +0330

    Update npm.ps1

--- npm.ps1
+++ npm.ps1
@@ -1,9 +1,12 @@
+##### JS Packages (Cross Platform, Open Source, CLI)
 npm install -g @angular/cli
 npm install -g moment
 npm install -g jquery
+npm install -g ts-node
 npm install -g typescript
 npm install -g primeng
 npm install -g @progress/kendo-ui
 npm install -g bootstrap
 npm install -g ngx-bootstrap
 npm install -g gulp-cli
+npm install -g electron
