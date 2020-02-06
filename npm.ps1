ac09170875a2e8182058cefb6359d344ae7d9b1d
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-07 00:40:10 +0330

    Update npm.ps1

--- npm.ps1
+++ npm.ps1
@@ -5,7 +5,27 @@ npm install -g jquery
 npm install -g @angular/cli
 npm install -g electron
 
+##### CSS Framework
+npm install -g bootstrap
+npm install -g material-components-web
+npm install -g semantic-ui
+
+##### UI Framework
+npm install -g jquery-ui
+npm install -g @angular/material
+npm install -g ngx-bootstrap
+npm install -g primeng
+npm install -g @progress/kendo-ui
+
+##### UI Widget
+npm install -g select2
+npm install -g jtable
+npm install -g formBuilder
+npm install -g jQuery-QueryBuilder
+npm install -g font-awesome
+
 ##### JS Library
+npm install -g webpack
 npm install -g parsleyjs
 npm install -g moment
 npm install -g lodash
@@ -13,22 +33,8 @@ npm install -g underscore
 npm install -g jquery-validation
 npm install -g grunt
 npm install -g gulp-cli
-npm install -g moment
 npm install -g ts-node
 npm install -g typescript
 
-##### UI Framework
-npm install -g ngx-bootstrap
-npm install -g primeng
-npm install -g @progress/kendo-ui
-npm install -g jquery-ui
-
-##### UI Widget
-npm install -g select2
-npm install -g jtable
-npm install -g formBuilder
-npm install -g jQuery-QueryBuilder
-
-##### CSS Framework
-npm install -g bootstrap
-
+##### HTML Template (Admin Panel)
+https://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469
