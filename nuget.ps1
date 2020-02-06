03b0f03982f63de1347167d7f91ac78ca4c78e05
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-06 20:17:43 +0330

    Update nuget.ps1

--- nuget.ps1
+++ nuget.ps1
@@ -1,2 +1,6 @@
+##### Install By Nuget CLI
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Newtonsoft.Json 
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Elmah
+
+##### Install By PowerShell
+Install-Package Microsoft.AspNet.SignalR
