05d80016e23cc3f405dc29493e0993630434e386
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-06 23:37:37 +0330

    Update npm.ps1

--- npm.ps1
+++ npm.ps1
@@ -1,16 +1,34 @@
 ##### JS Packages (Cross Platform, Open Source, CLI)
-npm install -g @angular/cli
-npm install -g moment
+
+##### JS Framework
 npm install -g jquery
+npm install -g @angular/cli
+npm install -g electron
+
+##### JS Library
+npm install -g parsleyjs
+npm install -g moment
+npm install -g lodash
+npm install -g underscore
+npm install -g jquery-validation
+npm install -g grunt
+npm install -g gulp-cli
+npm install -g moment
 npm install -g ts-node
 npm install -g typescript
+
+##### UI Framework
+npm install -g ngx-bootstrap
 npm install -g primeng
 npm install -g @progress/kendo-ui
-npm install -g bootstrap
-npm install -g ngx-bootstrap
-npm install -g gulp-cli
-npm install -g electron
-npm install -g grunt
+npm install -g jquery-ui
+
+##### UI Widget
+npm install -g select2
 npm install -g jtable
 npm install -g formBuilder
 npm install -g jQuery-QueryBuilder
+
+##### CSS Framework
+npm install -g bootstrap
+
