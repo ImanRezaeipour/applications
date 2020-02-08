31d1e2ab2fe1ac84f2cd48722a952c04744f0255
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-08 13:43:54 +0330

    Delete store.ps1

--- store.ps1
+++
@@ -1,5 +0,0 @@
-Get-AppxPackage
-Get-AppxPackage | Select Name , PackageFullName
-Get-AppxPackage -AllUsers | Remove-AppxPackage
-Get-AppXProvisionedPackage -online | Remove-AppxProvisionedPackage -online
-Get-AppXPackage *WindowsStore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppXManifest.xml"}
