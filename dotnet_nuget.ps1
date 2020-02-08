e78a4214f5430d67f83830b4b9da7ff93b84a0f7
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-08 15:20:03 +0330

    Update dotnet_nuget.ps1

--- dotnet_nuget.ps1
+++ dotnet_nuget.ps1
@@ -1,3 +1,9 @@
+##### Templates
+dotnet new mvc
+dotnet new console
+dotnet new blazor
+dotnet new webapi
+
 ##### Install By Nuget CLI
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Newtonsoft.Json 
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Elmah
