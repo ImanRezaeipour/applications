fcb383f3893252999826443a234636963d9a9366
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-08 21:34:49 +0330

    Update dotnet_nuget.ps1

--- dotnet_nuget.ps1
+++ dotnet_nuget.ps1
@@ -8,8 +8,9 @@ dotnet new webapi
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Newtonsoft.Json 
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Elmah
 
-##### Install By PowerShell
-Install-Package Microsoft.EntityFrameworkCore
+##### Library (Install By PowerShell)
+Install-Package xunit
+Install-Package IdentityServer4
 Install-Package Microsoft.AspNet.SignalR
 Install-Package Microsoft.AspNet.Identity.Core
 Install-Package structuremap
@@ -23,12 +24,22 @@ Install-Package ImageProcessor
 Install-Package FluentValidation
 Install-Package Lucene.Net
 Install-Package Elasticsearch.Net
-Install-Package Microsoft.AspNet.OData
 Install-Package ServiceStack
 Install-Package Quartz
 Install-Package Swashbuckle
 Install-Package EPPlus
+Install-Package MailKit
+Install-Package HangFire
 
+##### Library (Need Service)
+Install-Package Microsoft.EntityFrameworkCore
+Install-Package Microsoft.AspNet.OData
+Install-Package StackExchange.Redis
+Install-Package MongoDB.Driver
+Install-Package RabbitMQ.Client
 
-
-##### Needs Service
+##### Framework (Open Source, CLI)
+git clone https://github.com/aspnetboilerplate/aspnetboilerplate.git
+git clone https://github.com/abpframework/abp.git
+git clone https://github.com/nopSolutions/nopCommerce.git
+git clone https://github.com/dotnet/orleans.git
