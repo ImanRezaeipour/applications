c71a5ebca022cb001e53e8c50e70e2fc3cd9c3dd
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-08 20:45:06 +0330

    Update dotnet_nuget.ps1

--- dotnet_nuget.ps1
+++ dotnet_nuget.ps1
@@ -9,6 +9,26 @@ nuget install -OutputDirectory %UserProfile%\.nuget\packages Newtonsoft.Json
 nuget install -OutputDirectory %UserProfile%\.nuget\packages Elmah
 
 ##### Install By PowerShell
+Install-Package Microsoft.EntityFrameworkCore
 Install-Package Microsoft.AspNet.SignalR
+Install-Package Microsoft.AspNet.Identity.Core
+Install-Package structuremap
+Install-Package AutoMapper
+Install-Package Castle.Windsor
+Install-Package Microsoft.Owin.Security.OAuth
+Install-Package RestSharp
+Install-Package BouncyCastle
+Install-Package elmah
+Install-Package ImageProcessor
+Install-Package FluentValidation
+Install-Package Lucene.Net
+Install-Package Elasticsearch.Net
+Install-Package Microsoft.AspNet.OData
+Install-Package ServiceStack
+Install-Package Quartz
+Install-Package Swashbuckle
+Install-Package EPPlus
+
+
 
 ##### Needs Service
