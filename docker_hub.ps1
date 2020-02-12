a76f7851ade76c1dcd6f683dd4885e7b09d32166
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-12 12:01:56 +0330

    Update docker_hub.ps1

--- docker_hub.ps1
+++ docker_hub.ps1
@@ -1,1 +1,6 @@
 
+##### SQL Server container
+sudo docker pull mcr.microsoft.com/mssql/server
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Pa55w0rd' -p 1433:1433 -d --name mssqllinux mcr.microsoft.com/mssql/server
+
+##### Redis container
