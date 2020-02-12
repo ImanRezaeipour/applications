f6ed0d351a0c0d33d3d233f31b317899887e399f
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-02-12 12:46:44 +0330

    Update linux_apt-get.ps1

--- linux_apt-get.ps1
+++ linux_apt-get.ps1
@@ -1,9 +1,21 @@
+##### Install Snap
+sudo apt update
+sudo apt install -y snapd
+
 ##### PUBLIC
 sudo apt-get install -y gnome-tweak-tool
 sudo apt-get install -y openconnect
 
 ##### DEVELOP (Cross Platform, Open Source, CLI)
-sudo apt-get install -y powershell
-sudo apt-get install -y nodejs
-sudo apt-get install -y npm
-sudo apt-get install -y docker.io
+sudo snap install -y dotnet-sdk --classic
+sudo snap install -y dotnet-sdk --channel=2.2/stable --classic
+sudo apt-get install -y nuget
+sudo snap install -y rider --classic
+sudo snap install -y azuredatastudio
+
+sudo snap install -y node --classic
+sudo snap install -y code --classic
+sudo snap install -y datagrip --classic
+
+sudo snap install -y docker
+sudo snap install -y powershell --classic
