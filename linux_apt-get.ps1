f6eb2fe3637e371119ec013db6d084dc3a541766
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-03-08 19:41:45 +0330

    Update linux_apt-get.ps1

--- linux_apt-get.ps1
+++ linux_apt-get.ps1
@@ -6,18 +6,19 @@ sudo apt install -y snapd
 sudo apt-get install -y gnome-tweak-tool
 sudo apt-get install -y openconnect
 sudo snap install shotcut --classic
+sudo apt install ubuntu-restricted-extras
 
 
 ##### DEVELOP (Cross Platform, Open Source, CLI)
-sudo snap install -y dotnet-sdk --classic
-sudo snap install -y dotnet-sdk --channel=2.2/stable --classic
+sudo snap install -y dotnet-sdk --classic --channel=2.2/stable
 sudo apt-get install -y nuget
 sudo snap install -y rider --classic
 sudo snap install -y azuredatastudio
-
 sudo snap install -y node --classic
 sudo snap install -y code --classic
 sudo snap install -y datagrip --classic
-
 sudo snap install -y docker
 sudo snap install -y powershell --classic
+sudo apt-get install -y yarn
+sudo apt-get install -y git
+sudo snap install -y ffmpeg
