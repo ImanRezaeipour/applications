d9764abc2f64540c3eb1d3d16a05f080b0a0dc6a
Iman Rezaeipour <iman.rezaeipour@gmail.com>
2020-03-12 00:03:41 +0330

    Update docker_hub.ps1

--- docker_hub.ps1
+++ docker_hub.ps1
@@ -1,6 +1,8 @@
-
 ##### SQL Server container
 sudo docker pull mcr.microsoft.com/mssql/server
 sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Pa55w0rd' -p 1433:1433 -d --name mssqllinux mcr.microsoft.com/mssql/server
+sudo docker exec -it mssqllinux mkdir /var/opt/mssql/backup
+sudo docker cp ~/Azure/LastBK.bak mssqllinux:/var/opt/mssql/backup
+sudo docker exec -it mssqllinux /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P 'Pa55w0rd' -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/LastBK.bak"' | tr -s ' ' | cut -d ' ' -f 1-2
 
 ##### Redis container
